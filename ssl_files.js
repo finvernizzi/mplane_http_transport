49cecb1140a5940a7058804130898bb61caffd7a
invernizzi <fabrizio.invernizzi@telecomitalia.it>
2014-10-03 07:58:43 +0200

    Better comments in ssl_files

--- ssl_files.js
+++ ssl_files.js
@@ -1,6 +1,6 @@
 /**
  * This module manages external CA or keys
- * It is simply a wrapper of basic FS functions
+ * It is simply a wrapper of basic FS functions for simpler HTTPS options
  *
  * @type {readCaChain}
  */
@@ -10,7 +10,11 @@ var fs = require('fs');
 module.exports.readCaChain = readCaChain;
 module.exports.readFileContent = readFileContent;
 
-// Given an array of file name (complete path) or a single file (complete path), return an array of ca for use in https options
+/**
+ * Given an array of file name (complete path) or a single file (complete path), return an array of ca for use in https options
+ * @param caChain can be a file full path or an array of files (full path) if you are using a CA chain.
+ * @returns {*} the CA cahin to be used as options in https requests
+ */
 function readCaChain(caChain){
     var ret = [];
     if (caChain instanceof Array){
@@ -36,7 +40,11 @@ function readCaChain(caChain){
     }
     return ret;
 }
-// This is for reading cert and keys
+/**
+ * Reads cert and keys
+ * @param fileName (full path)
+ * @returns {*}
+ */
 function readFileContent(fileName){
     try{
         return(fs.readFileSync(fileName , "utf-8"));
